521ce71365e12728aa2017a8b5b33ea25b24956f
mharoot <michaelharootoonyan@yahoo.com>
2017-12-03 20:39:12 -0800

    commented out for presentation needs updates

--- error404.php
+++ error404.php
@@ -1,3 +1,5 @@
+<?php include_once 'view/templates/header.php'; ?>
+
 <h1> Error 404 </h1>
 <p>
 <?php
@@ -7,3 +9,4 @@ if ( isset($_SESSION['error_message']) )
 session_write_close();
 ?>
 </p>
+<?php include_once 'view/templates/footer.php'; ?>
--- index.php
+++ index.php
@@ -1,16 +1,5 @@
 <?php 
-
-
-
-include_once("controller/Controller.php");
-$SUPER_CONTROLLER = new Controller();
-$SUPER_CONTROLLER->invoke();
-	// include_once("controller/Books.php");
-	// include_once("controller/Authors.php");
-	// $books_controller = new Books();
-	// $books_controller->invoke();
-
-	// $authors_controller = new Authors();
-	// $authors_controller->invoke();
-
+	include_once("controller/Controller.php");
+	$SUPER_CONTROLLER = new Controller();
+	$SUPER_CONTROLLER->invoke();
 ?>
